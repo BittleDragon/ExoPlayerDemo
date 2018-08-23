440658c898ef4d82752ebebe04cbaf5f78cc7e88
bamboo <rxt@100fen.cn>
2018-08-23 17:31:41 +0800

    界面创建

--- app/src/main/java/com/rxt/exoplayerdemo/MainActivity.java
+++ app/src/main/java/com/rxt/exoplayerdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.rxt.exoplayerdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_play)
+    public void onViewClicked() {
+
     }
 }
